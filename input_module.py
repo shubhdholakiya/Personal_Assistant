7a220a54b0c197cf76c5bb3ea8a6db97779ec46e
Shubh-coder007 <shubhdholakiya>
2021-01-09 17:35:04 +0530

    assistant_details.py is also added

---
+++ input_module.py
@@ -0,0 +1,6 @@
+from assistant_details import name
+
+
+def take_input():
+    i = input(name+": ")
+    return i
--- start.py
+++ start.py
@@ -1,7 +1,10 @@
 # This is Self Learning Personal Assistant, it's name is Sam
 from input_module import take_input
 from process_module import process
+from output_module import output
 while True:
     i = take_input()
     o = process(i)
+    output(o)
+
 
